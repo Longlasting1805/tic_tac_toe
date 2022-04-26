1a8e57f03169d51737c390a13147f7c442f7e88e
Akande Al-ameen <longlasting1805@gmail.com>
2022-04-26 13:54:24 +0100

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+const style = {
+	background: "lightblue",
+	border: "2px solid darkblue",
+	fontSize: "30px",
+	fontWeight: "800",
+	cursor: "pointer",
+	outline: "none",
+};
+
+
+const Square = (props) => (
+	<button onClick={props.onClick}>{props.value}</button>
+);
+
+export default style
